f4474145a5d01a1d7bd35f5e6011c4c3f2f07318
Syed Tajamul Hussain <syehussa0@publicisgroupe.net>
2019-09-22 22:31:57 +0530

    change in base class

--- src/main/java/com/API/Base/BaseClass.java
+++ src/main/java/com/API/Base/BaseClass.java
@@ -4,7 +4,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
-
+//
 public class BaseClass {
 	
 	public int Response_Status_Code_200 =200;
